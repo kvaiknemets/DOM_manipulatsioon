24dfe4d7214c77c86e355939f70b02af81e1babc
Kärt <k.vaiknemets@voco.ee>
2024-09-24 15:44:20 +0300

    Ülesanded

--- script.js
+++ script.js
@@ -57,3 +57,97 @@ magicButton.addEventListener('mouseover',(event) => {
 magicButton.addEventListener('mouseout',(event) => {
     event.target.style.backgroundColor = 'white'
 })
+
+// Event listenerid 24. sept
+
+let count = 0
+
+const numberCount = document.getElementById('number')
+
+const liitmiseNupp = document.getElementById('liida')
+const lahutamiseNupp = document.getElementById('lahuta')
+const tühistamiseNupp = document.getElementById('tühista')
+
+liitmiseNupp.onclick = () => {
+    count++
+    numberCount.innerHTML = count
+}
+
+lahutamiseNupp.onclick = () => {
+    count--
+    if (count >= 0) {
+    numberCount.innerHTML = count
+    }
+}
+
+tühistamiseNupp.onclick = () => {
+    count = 0
+    numberCount.innerHTML = count
+}
+
+// Minu versioon - lihtsam lahendus:
+ playground.addEventListener('click', () => {
+    alert('Div clicked!')
+});
+
+// Õpetaja lahendus (pikad jooned ei tööta)
+//const element = document.querySelectorAll('div')
+
+//console.log(element)
+
+/*document.addEventListener('click', (event) => {
+    console.log(event, event.target)
+    if(event.target.nodeName === 'DIV' ││ event.target.parentNode.nodeName === "DIV"){
+        alert('Div clicked')
+    }
+})
+*/
+
+const data = [
+{
+    name: "Banaan",
+    price: 4,
+},
+{
+    name: "Arbuus",
+    price: 3,
+},
+{
+    name: "Kiivi",
+    price: 2,
+},
+{
+    name: "Viinamari",
+    price: 2.5,
+},
+{
+    name: "Maasikad",
+    price: 4,
+},
+];
+
+const tabeligenereerimine = () => {
+
+
+const tabeliKeha = document.querySelector("tbody");
+
+    for (let i = 0; i < data.length; i++) {
+    //create a table row
+    const row = document.createElement("tr");
+
+    for(let j = 0; j < 2; j++) {
+        //create cells
+        const cell = document.createElement("td")
+        if (j === 0){
+            cell.append(data[i].name);
+        } else {
+            cell.append(data[i].price);
+        }
+        row.append(cell);
+    }
+
+        tabeliKeha.append(row);
+    }
+};
+
+tabeligenereerimine();
